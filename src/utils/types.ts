cb9b3a5cf6b3a216d04340cf1f00392ec8b5cd99
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-03-23 09:55:39 -0500

    ⚡️ Grade and Modal Types
    Added Grade and Modal Info Types in separated .ts files.

---
+++ src/utils/types.ts
@@ -0,0 +1,12 @@
+export type Grade = {
+    grade: string,
+    percentage: string,
+    color?: string,
+}
+
+export type ModalInfo = {
+    title: string,
+    gradeNeeded: number,
+    message: string,
+    color: string,
+}
